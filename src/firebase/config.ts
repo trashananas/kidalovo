f7e33fe3d040a57614102eec4ab4be7edf12e048
Глеб Дюмин <nervotrep22010@gmail.com>
2026-01-22 08:16:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4983417966-5c3c2",
+  "appId": "1:603897296892:web:81a270286f99d67ebc6479",
+  "apiKey": "AIzaSyAoh2WlH78dPBtI8JQL5nenBAIqLHwtbQo",
+  "authDomain": "studio-4983417966-5c3c2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "603897296892"
+};
